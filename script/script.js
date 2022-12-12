7f198b5e123f075e11f4a72e9f11c43661aac682
mukhriddin-dev <khodievm@gmail.com>
2022-12-13 01:12:57 +0500

    ok

--- script/script.js
+++ script/script.js
@@ -1,38 +1,51 @@
-let inputtag, hashtagArray, container, t;
+let inputtag, hashtagArray, container, t, sendForm, newInput;
 
 inputtag = document.querySelector('#hashtags');
 
 container = document.querySelector('.tag-containeruz');
 
+sendForm = document.querySelector('#sendForm');
+
+newInput= document.querySelector('#sends');
+
 hashtagArray = [];
 
 inputtag.addEventListener('keyup', (event) => {
-  
-    if (event.which == 13 && inputtag.value.length > 0) {
-      
+   event.preventDefault();
+
+   if (event.which == 13 && inputtag.value.length > 0) {
+
       var text = document.createTextNode(inputtag.value);
-      
+
       var p = document.createElement('p');
-      
+
       container.appendChild(p);
       hashtagArray.push(inputtag.value);
       console.log(hashtagArray)
       p.appendChild(text);
-      const jsonData=JSON.stringify(hashtagArray);
+      const jsonData = JSON.stringify(Object.assign({}, hashtagArray));
       console.log(jsonData);
-      
+
       p.classList.add('tag');
-      
+
       inputtag.value = '';
-      
-      
+      newInput.setAttribute('name', jsonData);
+
+
       let deleteTags = document.querySelectorAll('.tag');
-      
-      for(let i = 0; i < deleteTags.length; i++) {
-        deleteTags[i].addEventListener('click', () => {
-          container.removeChild(deleteTags[i]);
-          
-        });
+
+      for (let i = 0; i < deleteTags.length; i++) {
+         deleteTags[i].addEventListener('click', () => {
+            container.removeChild(deleteTags[i]);
+
+         });
       }
-    }
+   }
 });
+
+sendForm.addEventListener("submit", (e) => {
+   // e.preventDefault();
+   let nm = newInput.getAttribute("name");
+   console.log(nm);
+
+})
